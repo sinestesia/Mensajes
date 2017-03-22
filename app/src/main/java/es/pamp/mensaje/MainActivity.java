ad4775cc95530f8729888177baf28e242baf5472
sinestesia <reyesdelsuenio@gmail.com>
2017-03-22 20:45:03 +0100

    Login en ventana emergente

--- app/src/main/java/es/pamp/mensaje/MainActivity.java
+++ app/src/main/java/es/pamp/mensaje/MainActivity.java
@@ -1,10 +1,14 @@
 package es.pamp.mensaje;
 
 import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.IntDef;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -18,6 +22,7 @@ import android.widget.TimePicker;
 import android.widget.Toast;
 
 import java.util.Calendar;
+import java.util.logging.LogRecord;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -67,6 +72,20 @@ public class MainActivity extends AppCompatActivity {
                 mostrarSalir();
             }
         });
+        Button botonProgreso = (Button) findViewById(R.id.botonProgreso);
+        botonProgreso.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostrarProgreso();
+            }
+        });
+        Button botonLogin = (Button) findViewById(R.id.botonLogin);
+        botonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostrarLogin();
+            }
+        });
 
 
     }
@@ -74,14 +93,13 @@ public class MainActivity extends AppCompatActivity {
         onTimeListener = new TimePickerDialog.OnTimeSetListener() {
             @Override
             public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                Toast toast = Toast.makeText(getApplicationContext(), "Hora: " + " Minutos: ", Toast.LENGTH_SHORT);
+                Toast toast = Toast.makeText(getApplicationContext(), "Hora: " + hourOfDay + " Minutos: " + minute, Toast.LENGTH_SHORT);
                 toast.setGravity(Gravity.CENTER | Gravity.CENTER, 0, 0);
                 toast.show();
             }
         };
 
         muestraHora = new TimePickerDialog(context, onTimeListener,calendario.get(Calendar.HOUR_OF_DAY),calendario.get(Calendar.MINUTE),true);
-        //muestraHora = new TimePickerDialog(context, onTimeListener,1,1,true);
         muestraHora.show();
     };
 
@@ -110,5 +128,70 @@ public class MainActivity extends AppCompatActivity {
         AlertDialog alertDialog = alertDialogBuilder.create();
         alertDialog.show();
     };
+    private void mostrarProgreso() {
+        final ProgressDialog barraProgreso = new ProgressDialog(context);
+        barraProgreso.setMax(100);
+        barraProgreso.setTitle("Barra de progreso");
+        barraProgreso.setMessage("Cargando...");
+        barraProgreso.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        barraProgreso.show();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while (barraProgreso.getProgress() < barraProgreso.getMax()) {
+                        Thread.sleep(200);
+                    }
+                    barraProgreso.dismiss();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+        Handler handle = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                barraProgreso.incrementProgressBy(1);
+            }
+        };
+    }
+    private void mostrarLogin(){
+        final Dialog dialog = new Dialog(context);
+        dialog.setContentView(R.layout.custom);
+        dialog.setTitle("Login");
+        dialog.show();
+
+        Button dialogoAceptar = (Button) dialog.findViewById(R.id.botonAceptar);
+        dialogoAceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(context, Main2Activity.class);
+                String usuario =  ((EditText)dialog.findViewById(R.id.user)).getText().toString();
+                String pass = ((EditText)dialog.findViewById(R.id.pass)).getText().toString();
+                if (!usuario.equals("") && !pass.equals("") && usuario.equals(pass)){
+                    startActivity(i);
+                    finish();
+                }else{
+                    Toast toast = Toast.makeText(getApplicationContext(), "Contraseña incorrecta", Toast.LENGTH_SHORT);
+                    toast.setGravity(Gravity.CENTER | Gravity.CENTER, 0, 0);
+                    toast.show();
+                }
+
+
+            }
+        });
+        Button dialogoCancelar = (Button) dialog.findViewById(R.id.botonCancelar);
+        dialogoCancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+
+            }
+        });
+
+
+    }
 }
 
